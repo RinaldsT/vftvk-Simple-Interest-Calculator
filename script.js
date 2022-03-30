4d843affac1b79401e804b5125c9d868b50dcbb0
RinaldsT <rinalds@itisok.lv>
2022-03-30 19:29:38 +0000

    All is done

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
+//Focus cursor in Amount field
+window.onload = function() {
+    document.getElementById("principal").focus();
+}
+//Display slider value
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+//Construct message text
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,<br>\
+    at an interest rate of <mark>"+rate+"%</mark>.<br>\
+    You will receive an amount of <mark>"+interest+"</mark>,<br>\
+    in the year <mark>"+year+"</mark>";
+
 }
-        
+//Submit form & validate Amount
+function validateForm() {
+    var number = document.forms["mainform"]["principal"].value;
+    if (number <= "0") {
+      alert("Enter a positive number");
+      return true;
+    }
+    else
+    return false;
+}
